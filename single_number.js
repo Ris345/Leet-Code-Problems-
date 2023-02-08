bab3573d9c3651fdb6f4cfbe6f0a5b7f802d501a
Dorjee <rishav345@gmail.com>
2023-02-08 13:29:19 -0500

    cleanup

--- single_number.js
+++ single_number.js
@@ -1,7 +1,5 @@
 // solution passed 
-
 var singleNumber = function(nums) {
-    debugger; 
     // nums has duplicate items 
      // items are duplicate if the indexes are not the same as the previous index
   let obj = {}; 
@@ -19,29 +17,9 @@ var singleNumber = function(nums) {
 };
 
 
-
 console.log(singleNumber([1]));
 console.log(singleNumber(2, 2, 1));
 console.log(singleNumber([4, 1, 2, 1, 2])); 
-
-
-
-
-
- // for (let i = 0; i < nums.length; i++){
- //    for(let j =0; j < nums.length - 1; j++){
- //      if(nums[i] === nums[j + 1]){
- //        console.log(nums[i])
- //      }
- //    }
- // }
-
-// return nums.filter(function (x,y){
-//         return x
-//     })
-
-// it's better to use object because it keeps tracks of the key: value pairs that way we can find the frequency of each number. 
-
 
 
 
