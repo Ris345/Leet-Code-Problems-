eef979ad45a84615a142521b7da6ab1fc616264a
Dorjee <rishav345@gmail.com>
2023-02-20 16:44:42 -0500

    update

--- searchinsertposition.js
+++ searchinsertposition.js
@@ -1,4 +1,4 @@
-// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, 
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not,
 // return the index where it would be if it were inserted in order.
 
 // Input: nums = [1,3,5,6], target = 5
@@ -10,27 +10,26 @@
 // Input: nums = [1,3,5,6], target = 7
 // Output: 4
 
-// work in progress 
-var searchInsert = function(nums, target) {
-     // loop through the nums array 
-    for (let i = 0; i < nums.length; i++){
-        if (target === nums[i]) {
-            return i
+// work in progress
+var searchInsert = function (nums, target) {
+  // loop through the nums array
+  for (let i = 0; i < nums.length; i++) {
+    if (target === nums[i]) {
+      return i;
+    } else {
+      if (target - nums[i] === 1) {
+        if (target > nums[i]) {
+          nums.push(target);
+          return nums.indexOf(target);
         } else {
-            if (target > nums[i] || target < nums[i]) {
-                
-            }
+            // push the number with index 
         }
+      }
     }
-      // if target === nums[i]
-       // return index of nums
-    // else
-         // if the target number is not in the nums arr then we need to find a index for the target number to fit
-          // the target number must be greater or smaller than the last number in the array 
-           // if we know the last number is greater than the target then we can place it in the last index 
-             // else other wise we would have to check for every iteration until we find the number that is smaller than the target 
+  }
+    
 };
 
-const nums = [1,3,5,6]
+const nums = [1, 3, 5, 6];
 
-console.log(searchInsert(nums, 2)); 
+console.log(searchInsert(nums, 2));
