3a157c641ca554ce80f98ea6e2df6359e40b2ddb
Dorjee <rishav345@gmail.com>
2023-02-23 11:45:16 -0500

    putting the logic together

---
+++ longestcommonprefix.js
@@ -0,0 +1,12 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty string "".
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+var longestCommonPrefix = function(strs) {
+   // split the array trim all the extra space 
+    // loop through the modified array 
+     // check if each item in the array is equal to the second item like this [i] = [i + 1] 
+  
+  
+};
