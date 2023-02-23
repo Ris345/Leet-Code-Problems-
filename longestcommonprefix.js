533615b42fee332b4c51d3b85db0a8c8648aeea1
Dorjee <rishav345@gmail.com>
2023-02-23 12:14:54 -0500

    some progress

--- longestcommonprefix.js
+++ longestcommonprefix.js
@@ -3,10 +3,21 @@
 // Input: strs = ["flower","flow","flight"]
 // Output: "fl"
 
+// work in progress
 var longestCommonPrefix = function(strs) {
    // split the array trim all the extra space 
-    // loop through the modified array 
+     // filter and remove and extra white space 
+    // loop through the modified array
      // check if each item in the array is equal to the second item like this [i] = [i + 1] 
-  
-  
+    const item = strs.toString().trim().split('');
+    const modifiedArr = item.filter((x) => {
+      return x !== ","; 
+    })
+    for (let i = 0; i < modifiedArr.length; i++){
+        for (let j = 0; j < modifiedArr.length; j++){
+            if (modifiedArr[i] === modifiedArr[j + 1]) {
+                return modifiedArr[i]; 
+            }
+        }
+    }
 };
