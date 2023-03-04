0703b66209b1f6d11a500334dab2e934f213fc85
Dorjee <rishav345@gmail.com>
2023-03-04 12:54:39 -0500

    new-problem

---
+++ AddBinary.js
@@ -0,0 +1,22 @@
+// Given two binary strings a and b, return their sum as a binary string.
+// Input: a = "11", b = "1"
+// Output: "100"
+// Input: a = "1010", b = "1011"
+// Output: "10101"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
