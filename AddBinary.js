c773e9f4132c477c8f69daff39642cffc8cdc21a
Dorjee <rishav345@gmail.com>
2023-03-07 11:11:37 -0500

    trying

--- AddBinary.js
+++ AddBinary.js
@@ -4,7 +4,21 @@
 // Input: a = "1010", b = "1011"
 // Output: "10101"
 
+var addBinary = function (a, b) { 
+  let carry = 0; 
+  let result = 0; 
+  // binary numbers are only 1 & 0 anything greater than that gets conversted into 0 || 1  
+    // test case 1 + 11 = 100 --> step [1] 1 + 1  = 2 carry over 1 and add 0 there instead and add 0 for next 1 + 1  = 0 then put the final remainning 1 at the end
+     //
+    
+    // if sum > 3 invalid
+      // sum > 2 || sum > 1 show 0 but return 1
+       //
 
+ // so 1 + 1 = 2 ---> carry over 1 but should be 0 and then move that one to another number which makes it 0 and again an fonally number 1 gets added
+    
+    
+};
 
 
 
