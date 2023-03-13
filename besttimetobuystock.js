04f73c592038e4b1310c2fb06f1d615c294bc3ce
Dorjee <rishav345@gmail.com>
2023-03-13 11:31:32 -0400

    made some progress

---
+++ besttimetobuystock.js
@@ -0,0 +1,32 @@
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+
+// 
+
+
+var maxProfit = function (prices) {
+    const  prices = [7,1,5,3,6,4]
+    var maxProfit = function (prices) {
+      let arr = []; 
+      debugger; 
+      // run a for loop iterating over each item in the array 
+       //  choose a day to buy and a day to sell the stock 
+         // run a for loop 
+           for (let i = 0; i < prices.length; i++){
+              for (let j = 0; j < prices.length - 1; j++){
+                 const highNum = prices[i] - prices[j + 1]
+                 arr.push(highNum)
+                let largeNum = (Math.max(...arr)) - 1
+               return largeNum;      
+              }     
+         } 
+    };
+    
+    
+
+};
+
