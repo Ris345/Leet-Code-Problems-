bb09c70b5cb1c4921384b5255ecda9720d5a41e7
Dorjee <rishav345@gmail.com>
2023-03-13 11:32:26 -0400

    removed outer function

--- besttimetobuystock.js
+++ besttimetobuystock.js
@@ -8,25 +8,21 @@
 // 
 
 
+const  prices = [7,1,5,3,6,4]
 var maxProfit = function (prices) {
-    const  prices = [7,1,5,3,6,4]
-    var maxProfit = function (prices) {
-      let arr = []; 
-      debugger; 
-      // run a for loop iterating over each item in the array 
-       //  choose a day to buy and a day to sell the stock 
-         // run a for loop 
-           for (let i = 0; i < prices.length; i++){
-              for (let j = 0; j < prices.length - 1; j++){
-                 const highNum = prices[i] - prices[j + 1]
-                 arr.push(highNum)
-                let largeNum = (Math.max(...arr)) - 1
-               return largeNum;      
-              }     
-         } 
-    };
-    
-    
-
+  let arr = []; 
+  debugger; 
+  // run a for loop iterating over each item in the array 
+   //  choose a day to buy and a day to sell the stock 
+     // run a for loop 
+       for (let i = 0; i < prices.length; i++){
+          for (let j = 0; j < prices.length - 1; j++){
+             const highNum = prices[i] - prices[j + 1]
+             arr.push(highNum)
+            let largeNum = (Math.max(...arr)) - 1
+           return largeNum;      
+          }     
+     } 
 };
+
 
