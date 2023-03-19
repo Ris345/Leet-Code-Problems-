9df3d944e8bf2e872eddac8b776ac1cbd621f082
Dorjee <rishav345@gmail.com>
2023-03-19 11:44:13 -0400

    still working through it

--- majorityElement.js
+++ majorityElement.js
@@ -5,29 +5,39 @@
 //  test cases: Input: nums = [3,2,3]
 // Output: 3
 
+
+// work in progress 
 var majorityElement = function (nums) {
+    // declare an empty object 
     let similarNum = {};
+    let sameNum = 0
        debugger; 
        // nested loops i and j loops 
         // if items similar then return the index 
           // return the number that is occuring most frequently 
-        for (let i = 0; i < nums.length; i++){  //O(n)
-          // for edge cases return the if the index is 0
+        for (let i = 0; i < nums.length; i++){   //O(n)
+          // for edge cases return the if index 0 item 
           if(nums.length === 1){
             return nums[i]
             // go along with the next loop 
           } else {
               for(let j = i + 1; j < nums.length; j++){   //O(n)
-             // numbers are equal to each other and their length in the array is greater than the other number return 
+                // numbers are equal to each other and their length in the array is greater than the other number return 
              if(nums[i] === nums[j]) {
+                 sameNum = nums[i]
+                 console.log(sameNum)
                  similarNum[nums[i]] = i
              } 
            }
          }
       }
-    for (let key in similarNum){ // O(n)
-     return Math.max(key) 
+    // console.log(similarNum)
+    for (let key in similarNum){  // O(n)
+     return similarNum;
+      //  return the num with highest frequency 
+       // 
     }
   };
+  
   
   
