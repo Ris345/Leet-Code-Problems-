57d17585d96502db7b6ebbba204c1f478cd74e87
Dorjee <rishav345@gmail.com>
2023-03-19 09:10:22 -0400

    new problem

--- datastructures.js
+++ datastructures.js
@@ -2,38 +2,37 @@
 // const arr2 = ['z,', 'y' , 'x']
 
 // // function similarItem(){
-// //    debugger; 
-// //   // loop for arr1 
+// //    debugger;
+// //   // loop for arr1
 // //    for (let i = 0; i < arr1.length; i++){
 // //      for (let j = 0; j < arr2.length; j++){
 // //        if (arr1[i] == arr2[j]){
-// //      return true; 
-// //        } 
+// //      return true;
+// //        }
 // //      }
 // //    }
-// //   return false 
+// //   return false
 // // }
 
 // // console.log(similarItem())
 
 // // function similarItem(arr1, arr2){
-// //   let map = {}; 
+// //   let map = {};
 // //   for(let i = 0; i < arr1.length; i++){
 // //     if(!map[arr1[i]]){
 // //       const item = arr1[i]
-// //       map[item] = true; 
-// //     } 
+// //       map[item] = true;
+// //     }
 // //   }
 // //   for (let j = 0; j < arr2.length; j++){
 // //     if (map[arr2[j]]){
-// //        return true; 
+// //        return true;
 // //     }
 // //   }
 // //   return false;
 // // }
 
-// //  console.log(similarItem(arr1,arr2)); 
-
+// //  console.log(similarItem(arr1,arr2));
 
 // // function similarItem (arr1,arr2){
 // //   return arr1.some(item => arr1.includes(item))
@@ -42,7 +41,7 @@
 // // class Player {
 // //   constructor (name, type){
 // //     console.log(this)
-// //     this.name = name 
+// //     this.name = name
 // //     this.type = type
 // //   }
 // //   introduce (){
@@ -58,40 +57,39 @@
 // //   play (){
 // //     console.log(`WEEEEEEEEEEEEEE ${this.type}`)
 // //   }
-// // } 
+// // }
 
-
-// // // const wizard1 = new Wizard('Jinko', 'Darkness'); 
-// // // const wizard2 = new Wizard('Minko', 'Culzad'); 
+// // // const wizard1 = new Wizard('Jinko', 'Darkness');
+// // // const wizard2 = new Wizard('Minko', 'Culzad');
 
 // // class MyArray{
 // //   constructor(){
 // //     this.length = 0
-// //     this.data = {}; 
+// //     this.data = {};
 // //   }
 // //   get(index){
 // //     return this.data[index]
 // //   }
 // //   push(item){
-// //     this.data[this.length] = item; 
+// //     this.data[this.length] = item;
 // //     this.length++
-// //     return this.length; 
+// //     return this.length;
 // //   }
 // //   pop(){
 // //     const lastItem = this.data[this.length - 1]
-// //     delete this.data[this.length - 1]; 
+// //     delete this.data[this.length - 1];
 // //     this.length--
-// //     return lastItem; 
+// //     return lastItem;
 // //   }
 // //   delete(index){
-// //     const item = this.data[index]; 
-// //     this.shiftItems(index); 
+// //     const item = this.data[index];
+// //     this.shiftItems(index);
 // //   }
 // //   shiftItems(index){
 // //      for (let i = index; i < this.length - 1; i++){
 // //        this.data[i] = this.data[i + 1];
 // //      }
-// //     delete this.data[this.length - 1]; 
+// //     delete this.data[this.length - 1];
 // //     this.length--
 // //   }
 // // }
@@ -100,20 +98,19 @@
 // // newArray.push('coolbeans');
 // // newArray.push('jinkoCookie');
 // // newArray.push('butterLife')
-// // // newArray.pop(); 
-// // // newArray.pop(); 
-// // newArray.delete(0); 
-// // console.log(newArray); 
+// // // newArray.pop();
+// // // newArray.pop();
+// // newArray.delete(0);
+// // console.log(newArray);
 
-// // one way  of doing it.... 
-// function reverseStr (str){ 
-//   //always think about the edge cases 
-//   return str.split('').reverse().join(''); 
+// // one way  of doing it....
+// function reverseStr (str){
+//   //always think about the edge cases
+//   return str.split('').reverse().join('');
 // }
 // //console.log(reverseStr('Hi my name is Rishav'))
 
-
-// // another way of doing it 
+// // another way of doing it
 // function reverse (str){
 //   let string = ''
 //    for(let i = str.length - 1 ; i > 0; i--){
@@ -124,12 +121,12 @@
 
 // //console.log(reverse('Hi my name is Rishav'))
 
-// // modern way of approaching this problem 
-// const reverse1 = str => str.split('').reverse().join(''); 
+// // modern way of approaching this problem
+// const reverse1 = str => str.split('').reverse().join('');
 
 // //console.log(reverse1('Hi my name is Rishav'))
 
-// // even cooler approach 
+// // even cooler approach
 
 // const reverse2 = str => [...str].reverse().join('')
 
@@ -138,23 +135,20 @@
 //mergeSortedArrays([0,3,4,31], [4,6,30])
 //[0,3,4,4,6,30,31]
 
-// one way of doing it 
-function mergeSortedArrays(...arr){
-    if (!arr) return 'Try Again with the correct data type!'
-    const arrFlat = arr.flat(); 
-    return arrFlat.sort((x,y) => x - y )
- }
- 
- //console.log(mergeSortedArrays([0,3,4,31],[4,6,30]))
- 
- 
- //another way 
- function mergeSortedArrays2(...arr){
-    // edge cases 
-   if (arr.length === 0 ) return arr
-   return arr.flat().sort((x,y) => x - y);
- }
- 
- console.log(mergeSortedArrays2([0,3,4,31],[4,6,30]))
+// one way of doing it
+function mergeSortedArrays(...arr) {
+  if (!arr) return "Try Again with the correct data type!";
+  const arrFlat = arr.flat();
+  return arrFlat.sort((x, y) => x - y);
+}
 
+//console.log(mergeSortedArrays([0,3,4,31],[4,6,30]))
 
+//another way
+function mergeSortedArrays2(...arr) {
+  // edge cases
+  if (arr.length === 0) return arr;
+  return arr.flat().sort((x, y) => x - y);
+}
+
+console.log(mergeSortedArrays2([0, 3, 4, 31], [4, 6, 30]));
