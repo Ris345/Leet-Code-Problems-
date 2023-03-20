2e2fcf46cfe3f16faca7b176deb71d337177aa9f
Dorjee <rishav345@gmail.com>
2023-03-20 12:28:05 -0400

    lesson updates

--- datastructures.js
+++ datastructures.js
@@ -152,3 +152,34 @@ function mergeSortedArrays2(...arr) {
 }
 
 console.log(mergeSortedArrays2([0, 3, 4, 31], [4, 6, 30]));
+
+
+class HashTable {
+  constructor (size){
+    
+    this.data = new Array(size)
+  }
+  set fruit(item){
+  return this.item
+}
+
+_hash(key){
+
+  let hash = 0
+  for (let i = 0; i < key.length; i++){
+    hash = (hash + key.charCodeAt(i) * i ) % this.data.length 
+  }
+  return hash; 
+ }
+}
+
+const myHashTable = new HashTable(50); 
+myHashTable.set('grapes' ,10000)
+myHashtable.get('grapes')
+
+
+
+
+
+
+
--- majorityElement.js
+++ majorityElement.js
@@ -5,33 +5,36 @@
 //  test cases: Input: nums = [3,2,3]
 // Output: 3
 
-
 var majorityElement = function (nums) {
-  // declare an empty object 
+  // declare an empty object
   let similarNum = {};
-     debugger; 
-     // nested loops i and j loops 
-      // if items similar then return the index 
-        // return the number and pck that number into an object that is occuring most frequently 
-      for (let i = 0; i < nums.length; i++){   //O(n)
-        // for edge cases return the if index 0 item 
-        if(nums.length === 1){
-          return nums[i]
-          // go along with the next loop 
-        } else {
-            for(let j = i + 1; j < nums.length; j++){   //O(n)
-              // numbers are equal to each other and their length in the array is greater than the other number return 
-           if(nums[i] === nums[j]) {
-               similarNum[nums[i]] = i
-           } 
-         }
-       }
+  debugger;
+  // nested loops i and j loops
+  // if items similar then return the index
+  // return the number and pck that number into an object that is occuring most frequently
+  for (let i = 0; i < nums.length; i++) {
+    //O(n)
+    // for edge cases return the if index 0 item
+    if (nums.length === 1) {
+      return nums[i];
+      // go along with the next loop
+    } else {
+      for (let j = i + 1; j < nums.length; j++) {
+        //O(n)
+        // numbers are equal to each other and their length in the array is greater than the other number return
+        if (nums[i] === nums[j]) {
+          similarNum[nums[i]] = i;
+        }
+      }
     }
+  }
 
-  for (let key in similarNum){  // O(n)
-    let unpackedItem = Object.values(similarNum)
-    return unpackedItem.reduce((x,y) => {   // O(n)
-      return (x > y) ? parseInt(x): parseInt(y);
-    })
+  for (let key in similarNum) {
+    // O(n)
+    let unpackedItem = Object.values(similarNum);
+    return unpackedItem.reduce((x, y) => {
+      // O(n)
+      return x > y ? parseInt(x) : parseInt(y);
+    });
   }
 };
