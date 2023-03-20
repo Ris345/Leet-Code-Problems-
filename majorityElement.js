9475e5fa8595312354c48486d0591fe2f60d41d3
Dorjee <rishav345@gmail.com>
2023-03-20 08:52:02 -0400

    24/45 cases passed

--- majorityElement.js
+++ majorityElement.js
@@ -6,38 +6,32 @@
 // Output: 3
 
 
-// work in progress 
 var majorityElement = function (nums) {
-    // declare an empty object 
-    let similarNum = {};
-    let sameNum = 0
-       debugger; 
-       // nested loops i and j loops 
-        // if items similar then return the index 
-          // return the number that is occuring most frequently 
-        for (let i = 0; i < nums.length; i++){   //O(n)
-          // for edge cases return the if index 0 item 
-          if(nums.length === 1){
-            return nums[i]
-            // go along with the next loop 
-          } else {
-              for(let j = i + 1; j < nums.length; j++){   //O(n)
-                // numbers are equal to each other and their length in the array is greater than the other number return 
-             if(nums[i] === nums[j]) {
-                 sameNum = nums[i]
-                 console.log(sameNum)
-                 similarNum[nums[i]] = i
-             } 
-           }
+  // declare an empty object 
+  let similarNum = {};
+     debugger; 
+     // nested loops i and j loops 
+      // if items similar then return the index 
+        // return the number and pck that number into an object that is occuring most frequently 
+      for (let i = 0; i < nums.length; i++){   //O(n)
+        // for edge cases return the if index 0 item 
+        if(nums.length === 1){
+          return nums[i]
+          // go along with the next loop 
+        } else {
+            for(let j = i + 1; j < nums.length; j++){   //O(n)
+              // numbers are equal to each other and their length in the array is greater than the other number return 
+           if(nums[i] === nums[j]) {
+               similarNum[nums[i]] = i
+           } 
          }
-      }
-    // console.log(similarNum)
-    for (let key in similarNum){  // O(n)
-     return similarNum;
-      //  return the num with highest frequency 
-       // 
+       }
     }
-  };
-  
-  
-  
+
+  for (let key in similarNum){  // O(n)
+    let unpackedItem = Object.values(similarNum)
+    return unpackedItem.reduce((x,y) => {   // O(n)
+      return (x > y) ? parseInt(x): parseInt(y);
+    })
+  }
+};
