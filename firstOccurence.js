ee892855277dcc28bae9ef50b3a91bc8bdf1da13
Dorjee <rishav345@gmail.com>
2023-03-22 10:18:34 -0400

    haystack

---
+++ firstOccurence.js
@@ -0,0 +1,27 @@
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+
+// step 1 the parameter needle consists of a value we are looking in haystack 
+// step 2 if the value of needle item is in the haystack then return the index of that item 
+// step 3 else return - 1
+
+var strStr = function(haystack, needle) {
+    // we will loop throught the haystack 
+     // if needle is in the haystack
+       // return the index of the value of needle in haystack
+    // else
+      // return - 1
+    
+};
+
+
+
