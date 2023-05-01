f810f0f60fdaed4374ee3dc2fbe0021ceb5293bd
Dorjee <rishav345@gmail.com>
2023-05-01 10:57:17 -0400

    passive learning: data structures course

--- datastructures.js
+++ datastructures.js
@@ -274,35 +274,36 @@ function checkDuplicates(num) {
 
 // 10 ---> 5 -----> 16
 
-let mylinkedList = {
-  head: {
-    value: 10,
-    next: {
-      value: 5,
-      next: {
-        value: 16,
-        next: {
-          value: null,
-        },
-      },
-    },
-  },
-};
+// add a method prepend() to the linked list that adds a node to the beginning of the list
 
 class LinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      next: null,
+      next: null
     };
     this.tail = this.head;
     this.length = 1;
   }
   append(value) {
-    //     code
+    const newNode = {
+      value: value,
+      next: null
+    }
+    console.log(newNode)
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    //Code here
   }
 }
 
-let mylinkedlist = new LinkedList(10);
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);myLinkedList.prepend(1)
 
-// console.log(mylinkedlist)
+
+
--- uniqueChar.js
+++ uniqueChar.js
@@ -1,19 +1,39 @@
+// s = "loveleetcode";
+// var firstUniqChar = function (s) {
+//   let str = "";
+//   debugger;
+//   // unique char repeats only once
+//   // keep track of each item in a obj
+//   // push it using a for loop
+//   // using brute force
+//   for (let i = 0; i < s.length; i++) {
+//     for (let j = 1; j < s.length; j++) {
+//       if (s[i] === s[j]) {
+//         str += s[i];
+//       }
+//     }
+//     return i;
+//   }
+// };
+
+// console.log(firstUniqChar(s));
+
 s = "loveleetcode";
-var firstUniqChar = function (s) {
-  let str = "";
+var firstUniqeChar = function (s) {
   debugger;
-  // unique char repeats only once
-  // keep track of each item in a obj
-  // push it using a for loop
-  // using brute force
+  let obj = {};
+  // iterate through each item
+  //  if the index of the item is not the same as the previous item then item is not unique
   for (let i = 0; i < s.length; i++) {
-    for (let j = 1; j < s.length; j++) {
-      if (s[i] === s[j]) {
-        str += s[i];
-      }
+    //  keep track of where the item is getting stored in the index
+    console.log(s[i], i);
+    // if the previous index of the item is not equal to new index then the item does not exist
+    if (prevIdx != newIdx) {
+      //  then item is unqiue
+    } else {
+      // item is not unique
     }
-    return i;
   }
 };
 
-console.log(firstUniqChar(s));
+console.log(firstUniqeChar(s));
