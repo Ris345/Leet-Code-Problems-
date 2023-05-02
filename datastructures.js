316341a9e65080819e0fbc17f82c570b24c7ab2a
Dorjee <rishav345@gmail.com>
2023-05-02 16:10:51 -0400

    passive learning udemy

--- datastructures.js
+++ datastructures.js
@@ -276,11 +276,18 @@ function checkDuplicates(num) {
 
 // add a method prepend() to the linked list that adds a node to the beginning of the list
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      next: null
+      next: null,
     };
     this.tail = this.head;
     this.length = 1;
@@ -288,9 +295,9 @@ class LinkedList {
   append(value) {
     const newNode = {
       value: value,
-      next: null
-    }
-    console.log(newNode)
+      next: null,
+    };
+    console.log(newNode);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
@@ -298,12 +305,28 @@ class LinkedList {
   }
   prepend(value) {
     //Code here
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
   }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode != null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {}
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
-myLinkedList.append(16);myLinkedList.prepend(1)
-
-
-
+myLinkedList.append(16);
+myLinkedList.prepend(1);
