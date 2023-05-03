18b90d9830415627978a933933a25d9aa12e1355
Dorjee <rishav345@gmail.com>
2023-05-03 14:56:41 -0400

    solving the problem

--- uniqueChar.js
+++ uniqueChar.js
@@ -20,20 +20,20 @@
 
 s = "loveleetcode";
 var firstUniqeChar = function (s) {
-  debugger;
-  let obj = {};
-  // iterate through each item
-  //  if the index of the item is not the same as the previous item then item is not unique
-  for (let i = 0; i < s.length; i++) {
-    //  keep track of where the item is getting stored in the index
-    console.log(s[i], i);
-    // if the previous index of the item is not equal to new index then the item does not exist
-    if (prevIdx != newIdx) {
-      //  then item is unqiue
+  let obj = {}
+  for (let i = 0; i < s.length; i++){
+    if (obj[s[i]]) {
+      obj[s[i]] += 1
     } else {
-      // item is not unique
+      obj[s[i]] = 1
+    }
+    for (let j = 0; j < s.length; j++){
+      if (obj[s[i]] < 2) {
+        return i
+      } 
     }
   }
+  return - 1
 };
 
 console.log(firstUniqeChar(s));
