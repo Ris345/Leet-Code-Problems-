7af392910547e7245102986377c8ea876f035877
Dorjee <rishav345@gmail.com>
2023-06-18 16:13:49 -0400

    linked lists understanidng

--- datastructures.js
+++ datastructures.js
@@ -383,9 +383,15 @@ let testerList = {
 
 // console.log(testerList)
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   constructor(value) {
-    console.log(this.head)
     this.head = {
       value: value,
       next: null,
@@ -394,17 +400,61 @@ class LinkedList {
     this.length = 1;
   }
   append(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this;
   }
   prepend(value) {
-    
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
   }
 }
 let myLinkedList = new LinkedList(10);
@@ -412,3 +462,13 @@ let myLinkedList = new LinkedList(10);
 console.log(myLinkedList.append(5));
 
 console.log(myLinkedList.append(16));
+
+console.log(myLinkedList.prepend(12));
+
+console.log(myLinkedList.prepend(22));
+
+console.log(myLinkedList.printList());
+
+console.log(myLinkedList.insert(3, 56));
+
+console.log(myLinkedList.remove(3));
