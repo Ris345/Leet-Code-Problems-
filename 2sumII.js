c4e8d5615981fe02468f0c916684432fae22c237
Dorjee <rishav345@gmail.com>
2023-09-04 08:44:33 -0400

    regular woodshed

--- 2sumII.js
+++ 2sumII.js
@@ -1,58 +1,117 @@
-// const numbers = [2,3,4]
+// // const numbers = [2,3,4]
 
-// var twoSum = function (numbers, target) {
-//     debugger;
-//     // two pointer approach
-//     // decalre a left and right variable
-//     // while left < right keep adding until it finds the target
-//     // once target found return the index of the sum of the numbers
-//     let left = 0
-//     let sum = 0
-//     let right = numbers.length - 1
-//     while (left < right) {
-//         numbers[left]++
-//         sum = numbers[left] + numbers[right]
+// // var twoSum = function (numbers, target) {
+// //     debugger;
+// //     // two pointer approach
+// //     // decalre a left and right variable
+// //     // while left < right keep adding until it finds the target
+// //     // once target found return the index of the sum of the numbers
+// //     let left = 0
+// //     let sum = 0
+// //     let right = numbers.length - 1
+// //     while (left < right) {
+// //         numbers[left]++
+// //         sum = numbers[left] + numbers[right]
+// //     }
+// //     console.log(sum)
+// // };
+
+// // console.log(twoSum(numbers,6))
+
+// const numbers = [2, 3, 4];
+// // var twoSum = function(numbers, target) {
+// //     // two pointer approach
+// //     // decalre a left and right variable
+// //     // while left < right keep adding until it finds the target
+// //     // once target found return the index of the sum of the numbers
+// //     let left = 0
+// //     let right = numbers.length - 1
+// //     let sum = 0
+
+// //     while (left < right) {
+// //         sum = numbers[left] + numbers[right]
+// //         if (left < right) {
+// //             return [left + 1, right + 1]
+// //         } else if (left > right) {
+// //             left--
+// //         } else {
+// //             right++
+// //         }
+// //     }
+// // };
+
+// // console.log(twoSum(numbers, 6))
+
+// // var twoSum = function (numbers, target) {
+// //     debugger;
+// //     // two pointer approach
+// //     // decalre a left and right variable
+// //     // while left < right keep adding until it finds the target
+// //     // once target found return the index of the sum of the numbers
+// //     let left = 0
+// //     let right = numbers.length - 1
+// //     let sum = 0
+// //     while (left < right) {
+// //         sum = numbers[left] + numbers[right]
+// //         if (sum === target) {
+// //             return[left + 1, right + 1]
+// //         } else if (right > left) {
+// //             right--
+// //         } else {
+// //             left++
+// //         }
+// //     }
+// // };
+
+// // console.log(twoSum(numbers, 6))
+// let num = [5, 25, 75];
+
+// var twoSum = function (num, target) {
+//     debugger; 
+//   let left = 0;
+//   let right = num.length - 1;
+//   let sum = 0;
+//   while (left < right) {
+//       sum = num[left] + numbers[right];
+//       console.log(sum)
+//       console.log('left pointer', num[left])
+//       console.log('right pointer:', num[right] )
+//     if (sum === target) {
+//       return [left + 1, right + 1];
+//     } else if (right > left) {
+//       right--;
+//     } else {
+//       left++;
 //     }
-//     console.log(sum)
+//   }
 // };
 
-// console.log(twoSum(numbers,6))
+// console.log(twoSum(num, 100));
 
-const numbers = [2, 3, 4];
+
+
 // var twoSum = function(numbers, target) {
-//     // two pointer approach
-//     // decalre a left and right variable
-//     // while left < right keep adding until it finds the target
-//     // once target found return the index of the sum of the numbers
 //     let left = 0
 //     let right = numbers.length - 1
 //     let sum = 0
-
 //     while (left < right) {
-//         sum = numbers[left] + numbers[right]
-//         if (left < right) {
+//         sum = num[left] + num[right]
+//         if (sum === target) {
 //             return [left + 1, right + 1]
-//         } else if (left > right) {
-//             left--
+//         } else if (right > left) {
+//             right--
 //         } else {
-//             right++
+//             left++
 //         }
 //     }
 // };
 
-// console.log(twoSum(numbers, 6))
 
-// var twoSum = function (numbers, target) {
-//     debugger;
-//     // two pointer approach
-//     // decalre a left and right variable
-//     // while left < right keep adding until it finds the target
-//     // once target found return the index of the sum of the numbers
-//     let left = 0
+// var twoSum = function(numbers, target) {
+//     let left = 0 
 //     let right = numbers.length - 1
-//     let sum = 0
 //     while (left < right) {
-//         sum = numbers[left] + numbers[right]
+//         sum = num[left] + num[right]
 //         if (sum === target) {
 //             return[left + 1, right + 1]
 //         } else if (right > left) {
@@ -63,101 +122,76 @@ const numbers = [2, 3, 4];
 //     }
 // };
 
-// console.log(twoSum(numbers, 6))
-let num = [5, 25, 75];
-
-var twoSum = function (num, target) {
-    debugger; 
-  let left = 0;
-  let right = num.length - 1;
-  let sum = 0;
-  while (left < right) {
-      sum = num[left] + numbers[right];
-      console.log(sum)
-      console.log('left pointer', num[left])
-      console.log('right pointer:', num[right] )
-    if (sum === target) {
-      return [left + 1, right + 1];
-    } else if (right > left) {
-      right--;
-    } else {
-      left++;
-    }
-  }
-};
-
-console.log(twoSum(num, 100));
+// var twoSum = function(numbers, target) {
+//     let left = 0
+//     let right = numbers.length - 1
+//     let sum = 0
+//     while (left < right) {
+//         sum = numbers[left] + numbers[right]
+//         if (sum === target) {
+//             return[left + 1, right + 1]
+//         } else if (right > left) {
+//             right-- 
+//         } else {
+//             left++ 
+//         }
+//     }
+// };
 
 
+
+
+// var twoSum = function(numbers, target) {
+//     let left = 0 
+//     let right = numbers.length - 1
+//     let sum = 0 
+
+//     while (left < right) {
+//         sum = numbers[left] + nums[right]
+//         if (sum === target) {
+//             return[left + 1, right + 1]
+//         } else if (right > left) {
+//             right-- 
+//         } else {
+//             left++
+//         }
+//     }
+// };
+
+
+
+
+// correct version 
 
 var twoSum = function(numbers, target) {
     let left = 0
     let right = numbers.length - 1
-    let sum = 0
     while (left < right) {
-        sum = num[left] + num[right]
+        let sum = numbers[right] + numbers[left]
+        if (sum === target) {
+            return [left + 1, right + 1]            
+        } else if (sum > target) {
+            right--
+        } else {
+            left++
+        }
+    }
+};
+
+
+var twoSum = function(numbers, target) {
+    let left = 0 
+    let right = numbers.length - 1
+    while (left < right) {
         if (sum === target) {
             return [left + 1, right + 1]
-        } else if (right > left) {
-            right--
-        } else {
-            left++
-        }
-    }
-};
-
-
-var twoSum = function(numbers, target) {
-    let left = 0 
-    let right = numbers.length - 1
-    while (left < right) {
-        sum = num[left] + num[right]
-        if (sum === target) {
-            return[left + 1, right + 1]
-        } else if (right > left) {
-            right--
-        } else {
-            left++
-        }
-    }
-};
-
-var twoSum = function(numbers, target) {
-    let left = 0
-    let right = numbers.length - 1
-    let sum = 0
-    while (left < right) {
-        sum = numbers[left] + numbers[right]
-        if (sum === target) {
-            return[left + 1, right + 1]
-        } else if (right > left) {
-            right-- 
-        } else {
-            left++ 
-        }
-    }
-};
-
-
-
-
-var twoSum = function(numbers, target) {
-    let left = 0 
-    let right = numbers.length - 1
-    let sum = 0 
-
-    while (left < right) {
-        sum = numbers[left] + nums[right]
-        if (sum === target) {
-            return[left + 1, right + 1]
-        } else if (right > left) {
+        } else if (sum > target) {
             right-- 
         } else {
             left++
         }
     }
 };
-
 
 
 
--- validParenthesis.js
+++ validParenthesis.js
@@ -4,18 +4,78 @@ let parenthesis = {
   "[": "]",
 };
 
-var isValid = function (s) {
-    debugger;
-    let stack = [];
-    for (let k of s) {
-        if (parenthesis[k]) {
-            stack.push(parenthesis[k])
-        } else if (stack.length >  0 && stack[stack.length - 1] === k) {
+// var isValid = function (s) {
+//     debugger;
+//     let stack = [];
+//     for (let k of s) {
+//         if (parenthesis[k]) {
+//             stack.push(parenthesis[k])
+//         } else if (stack.length >  0 && stack[stack.length - 1] === k) {
+//             stack.pop()
+//         }
+//     }
+//     return stack.length === 0; 
+// }
+
+
+// console.log(isValid('[}'))
+
+
+
+
+var isValid = function(s) {
+    let container = {
+        '(': ')', 
+        '[': ']', 
+        '{':'}'
+    }
+    let stack = []
+    for (const c in s){
+        if(container[c]){
+            stack.push(container[c])
+        } else if (stack.length = 0 && stack[stack.length - 1] === c) {
             stack.pop()
         }
     }
+     return stack.length === 0
+};
+
+
+console.log(isValid('[]'))
+
+
+
+
+var isValid = function(s) {
+    let container = {
+        '(': ')', 
+        '[': ']', 
+        '{': '}'
+    }
+    const stack = []
+    for (const  ck in s) {
+        if (container[ck]) {
+              stack.push(ck)
+        } else if (stack.length === 0 && stack[stack.length - 1] === ck) {
+            stack.pop()
+          }
+    }
     return stack.length === 0; 
-}
+ };
 
 
-console.log(isValid('[}'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
