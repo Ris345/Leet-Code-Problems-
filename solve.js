3e50e796dfebcbd0d88c013df1ec0fcba65cec4c
Dorjee <rishav345@gmail.com>
2023-12-17 23:21:20 -0500

    regular problem solve

--- solve.js
+++ solve.js
@@ -50,33 +50,66 @@
 // console.log(multiply_number(8, 24)); 
 
 
+const optimize_multiply_number = (n, t) => {
+    if (t === 0) {
+        return 0
+    }
+    if (t === 1) {
+        return n; 
+    } else {
+        let product = n
+        for (let i = 1; i < t; i++){
+           product += n 
+    }
+    return product; 
+    }
+
+}
+console.log(optimize_multiply_number(8,4))
+
+
+// const multiply_number = (num, times) => {
+//     debugger; 
+//     if (times <= 0) {
+//         return 0; // If times is 0 or negative, the result should be 0
+//     } else {
+//         let result = num;
+//         for (let i = 1; i < times; i++) {
+//             result += num;
+//         }
+//         return result;
+//     }
+// }
+
+// console.log(multiply_number(8,2));
 
 // check if the given string has all unique characters, return true if all characters are unique, if there are any repetitions return false.
 // "cathy" -> true
 // "catchy" -> false
 
-let obj = {}
-const unique_char = (str) => {
-    debugger; 
-    // push it in the obj somehow 
-    for (let i = 0; i < str.length; i++){
-        if (obj[str[i]]) {
-            obj[str[i]] += 1
-        } else {
-            obj[str[i]] = 1
-        }    
-    }    
-    //  now loop through the object to find the frequnecy of the obj 
-    let frequnecy = Object.values(obj)
-    for (let i = 0; i < frequnecy.length; i++) {
-        if (frequnecy[i] > 1) {
-            return false
-        }
-    }
-    return true;
-}
+// let obj = {}
+// const unique_char = (str) => {
+//     debugger; 
+//     // push it in the obj somehow 
+//     for (let i = 0; i < str.length; i++){
+//         if (obj[str[i]]) {
+//             obj[str[i]] += 1
+//         } else {
+//             obj[str[i]] = 1
+//         }    
+//     }    
+//     //  now loop through the object to find the frequnecy of the obj 
+//     let frequnecy = Object.values(obj)
+//     for (let i = 0; i < frequnecy.length; i++) {
+//         if (frequnecy[i] > 1) {
+//             return false
+//         }
+//     }
+//     return true;
+// }
 
-console.log(unique_char('catchy')); 
+
+// console.log(unique_char('catchy')); 
 
 
 
